315d7ea00292a5961a9f025d213262b7960e1e43
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-10-06 22:26:35 +0300

    project setup and Building the Foundation section.

---
+++ src/App.jsx
@@ -0,0 +1,87 @@
+import { useState } from "react";
+import Form from "./components/Form";
+import MemoryCard from "./components/MemoryCard";
+
+export default function App() {
+  const [isGameOn, setIsGameOn] = useState(false);
+  const [emojisData, setEmojisData] = useState([]);
+
+  async function startGame(e) {
+    e.preventDefault();
+
+    try {
+      const response = await fetch(
+        "https://emojihub.yurace.pro/api/all/category/animals-and-nature"
+      );
+
+      if (!response.ok) {
+        throw new Error("Could not fetch data from API");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      const dataSlice = getDataSlice(data);
+
+      const emojisArray = getEmojisArray(dataSlice);
+
+      setEmojisData(emojisArray);
+      setIsGameOn(true);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  function getDataSlice(data) {
+    const randomIndices = getRandomIndices(data);
+    // ex: randomIndices = 10 22 30 50 80
+    console.log(randomIndices);
+    // return array of data emoji objects
+    const dataSlice = randomIndices.reduce((array, index) => {
+      array.push(data[index]);
+      return array;
+    }, []);
+    console.log(dataSlice);
+    return dataSlice;
+  }
+  // get randome emoji indices from api data(107 emoji) => (5 emoji)
+  function getRandomIndices(data) {
+    const randomIndicesArray = [];
+
+    for (let i = 0; i < 5; i++) {
+      const randomNum = Math.floor(Math.random() * data.length);
+      if (!randomIndicesArray.includes(randomNum)) {
+        randomIndicesArray.push(randomNum);
+      } else {
+        i--;
+      }
+    }
+
+    return randomIndicesArray;
+  }
+
+  // return final shuffiled and dublicated array of emojis
+  function getEmojisArray(data) {
+    const pairedEmojisArray = [...data, ...data];
+
+    for (let i = pairedEmojisArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = pairedEmojisArray[i];
+      pairedEmojisArray[i] = pairedEmojisArray[j];
+      pairedEmojisArray[j] = temp;
+    }
+
+    return pairedEmojisArray;
+  }
+
+  function turnCard() {
+    console.log("Memory card clicked");
+  }
+
+  return (
+    <main>
+      <h1>Memory</h1>
+      {!isGameOn && <Form handleSubmit={startGame} />}
+      {isGameOn && <MemoryCard handleClick={turnCard} data={emojisData} />}
+    </main>
+  );
+}
