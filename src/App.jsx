70d25ab06e8f8f315cf92a609964fcf831130cc2
M0hammedMahm0ud <moseleeeem12@gmail.com>
2025-10-08 19:09:03 +0300

    Detect mathing cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Form from "./components/Form";
 import MemoryCard from "./components/MemoryCard";
 
 export default function App() {
   const [isGameOn, setIsGameOn] = useState(false);
   const [emojisData, setEmojisData] = useState([]);
+  const [selectedCards, setSelectedCards] = useState([]);
+  const [matchedCards, setMatchedCards] = useState([]);
+
+  useEffect(() => {
+    if (
+      selectedCards.length === 2 &&
+      selectedCards[0].name === selectedCards[1].name
+    ) {
+      setMatchedCards((prev) => [...prev, ...selectedCards]);
+    }
+  }, [selectedCards]);
 
   async function startGame(e) {
     e.preventDefault();
@@ -73,8 +84,20 @@ export default function App() {
     return pairedEmojisArray;
   }
 
-  function turnCard() {
-    console.log("Memory card clicked");
+  function turnCard(index, name) {
+    const selectedCardEntry = selectedCards.find(
+      (emoji) => emoji.index === index
+    );
+
+    if (!selectedCardEntry && selectedCards.length < 2) {
+      setSelectedCards((prevSelectedCards) => [
+        ...prevSelectedCards,
+        { name, index },
+      ]);
+    } else if (!selectedCardEntry && selectedCards.length === 2) {
+      setSelectedCards([{ name, index }]);
+    }
+    console.log(selectedCards);
   }
 
   return (
